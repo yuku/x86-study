58574b6bde06c0dc01a78257e9b82865799e5b39
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-05-02 18:34:45 +0900

    Add Emulator structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+const REGISTERS_COUNT: usize = 8;
+const MEMORY_SIZE: usize = 1024 * 1024;
+
+#[allow(dead_code)]
+pub struct Emulator {
+    registers: [u32; REGISTERS_COUNT],
+    eflags: u32,
+    eip: u32,
+    memory: [u8; MEMORY_SIZE],
+}
+
+impl Emulator {
+    pub fn new() -> Emulator {
+        Emulator {
+            registers: [0; REGISTERS_COUNT],
+            eflags: 0,
+            eip: 0,
+            memory: [0; MEMORY_SIZE],
+        }
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+extern crate px86;
+
+fn main() {
+    let mut emu = px86::Emulator::new();
+}
