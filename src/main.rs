92835f9e8fe63987fc3570421129dd288718b634
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-05-05 21:42:29 +0900

    Refactor a bit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 extern crate px86;
 
 fn main() {
-    let mut emu = px86::Emulator::new(0x7c00, 0x7c00);
-
-    emu.load("files/helloworld.bin");
-    emu.run();
+    px86::emulate("files/helloworld.bin");
 }
