c5156a366eb62097c5b25bdc4a58cb93ae511670
Yuku TAKAHASHI <taka84u9@gmail.com>
2016-05-07 03:17:19 +0000

    Give the path for a binary file to program as a command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 extern crate px86;
 
+use std::env;
+
 fn main() {
-    px86::emulate("files/helloworld.bin");
+    if let Some(path) = env::args().nth(1) {
+        px86::emulate(&path);
+    } else {
+        println!("usage: cargo run path/to/binary");
+    }
 }
